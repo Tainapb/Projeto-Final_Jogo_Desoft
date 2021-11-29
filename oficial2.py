c7b1dccd11229b48f721bb3b0ac6aedb9f771aa0
Tainá <Tainapb@al.insper.edu.br>
2021-11-29 19:37:08 -0300

    musics

--- oficial2.py
+++ oficial2.py
@@ -121,8 +121,8 @@ cinza =(127,127,127)
 rosa=(200, 0, 100)
 preto=(0,0,0)
 #carregando os sons do jogo 
-som_pulo = pygame.mixer.Sound('Projeto-final/musics/pulo.wav')
-som_queda = pygame.mixer.Sound('Projeto-final/musics/queda1.wav')
+som_pulo = pygame.mixer.Sound('musics/pulo.wav')
+som_queda = pygame.mixer.Sound('musics/queda1.wav')
 #definindo as fontes do texto 
 fonte=pygame.font.SysFont("inkfree", 25, bold=True, italic=True )  # vai definir a fonte do texto que aparecerá na tela 
 fonte2=pygame.font.SysFont("inkfree", 40, bold=True, italic=True )
@@ -188,9 +188,6 @@ rol = 0
 for i in range(lives+1):
     cora=Vidas(i*50,50)
     vidas.add(cora)  
-    
-'''colher=Colher(imagem_colher)
-all_colheres.add(colher) '''
 
 game=True
 
